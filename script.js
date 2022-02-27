0e947aa37d78658e53b6426591e9cff530ee1460
Wilson Júnior <willnapolitano21@gmail.com>
2022-02-26 21:14:52 -0300

    Create script.js file

---
+++ script.js
@@ -0,0 +1,14 @@
+const baseURL = "https://pokeapi.co/api/v2/pokemon/"
+
+let pokemon;
+
+function requestPokeInfo(url, name){
+    fetch(url + name)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.log(err))
+}
+
+requestPokeInfo(baseURL, 'pikachu')
